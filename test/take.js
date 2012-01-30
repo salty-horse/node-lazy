bc6928d88c37a0d8f8385f57511a2d16e1d55b20
Ori Avtalion <ori@avtalion.name>
2012-01-30 22:55:31 +0200

    Add tests for take(), one failing

--- test/take.js
+++ test/take.js
@@ -8,19 +8,26 @@ function range(i, j) {
     return r;
 }
 
-exports['take'] = function () {
+take_expect = function (amount, pool, expected, emit_end) {
+    if (!emit_end) emit_end = false;
+
     var lazy = new Lazy;
     var data = [];
     var executed = 0;
-    lazy.take(6).join(function (xs) {
-        assert.deepEqual(xs, range(0,6));
+    lazy.take(amount).join(function (xs) {
+        assert.deepEqual(xs, range(0,expected));
         executed++;
     });
 
-    range(0,10).forEach(function (x) {
+    range(0,pool).forEach(function (x) {
         lazy.emit('data', x);
     });
+    if (emit_end) lazy.emit('end');
 
     assert.equal(executed, 1, 'join executed incorrectly');
 }
 
+exports['take_6_of_10'] = take_expect(6, 10, 6);
+exports['take_6_of_6'] = take_expect(6, 6, 6);
+exports['take_6_of_6_end'] = take_expect(6, 6, 6, true);
+exports['take_10_of_6_end'] = take_expect(10, 6, 6, true);
